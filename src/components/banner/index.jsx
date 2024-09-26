aeaced8c8ba5d3209d2893931cebfce39a49cdc2
Fayziyev2218 <fayziyev2218@gmail.com>
2024-09-26 11:12:04 +0500

    ADD

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 import bannerDelevirCar from '/public/bannerDelevirCar.png'
 import bunnerCarpet from '/public/bannerCarpet3.png'
 
-export default function Banner({title,text,button,text3,text4,tel1,tel2,address,img,img2,infoTel}){
-    return(
+export default function Banner({ title, text, button, text3, text4, tel1, tel2, address, img, img2, infoTel }) {
+    return (
         <div className="bg-[url('/bannerBG.png')] bg-no-repeat bg-cover bg-center relative rounded-[32px] px-[64px] pt-[66px] pb-[83px] max-phone-sm:pb-[50px] flex items-center justify-between max-lg:px-[30px] max-table-850:flex-col max-table-850:gap-y-6 max-phone-sm:px-[20px] overflow-x-hidden">
             <div className='w-[60%] max-lg:w-[80%] max-table-850:w-full'>
                 <h3 className="text-white text-[42px] font-bold leading-[50px] mb-[14px] tracking-tight-custom max-sm:text-[37px] max-sm:leading-[40px] max-phone:text-[34px] max-phone:leading-[37px]">{title}</h3>
@@ -21,7 +21,7 @@ export default function Banner({title,text,button,text3,text4,tel1,tel2,address,
                         <p className='text-white text-[14px] font-normal leading-normal tracking-[-0.56px] opacity-[0.6] mb-[11px]'>{text4}</p>
                         <p className='text-white text-[20px] font-medium leading-normal tracking-[-0.8px]'>{address}</p>
                     </div>
-                </div> } 
+                </div>}
             </div>
 
             <div className='w-[46%] max-sm:w-full'>
@@ -34,13 +34,13 @@ export default function Banner({title,text,button,text3,text4,tel1,tel2,address,
 
                 {img2 && <div className='w-[650px] max-phone-sm:w-[500px] h-[279px] max-sm:h-auto translate-x-[95px] translate-y-[55px] max-xl:translate-x-[45px] max-md:translate-x-[40px] max-md:translate-y-0'>
                     <Image
-                            src={bunnerCarpet}
-                            alt="bunnerCarpet"
-                        />
+                        src={bunnerCarpet}
+                        alt="bunnerCarpet"
+                    />
                 </div>}
             </div>
 
-           
+
         </div>
     )
 }
